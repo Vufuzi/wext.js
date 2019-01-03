23633fa98d6ec7c41957087b1b0d2b89cfd9027a
Jeremy Karlsson <karlsson@jeremy.se>
2019-01-03 20:11:08 +0100

    Update test to include client side router

--- test/test.mjs
+++ test/test.mjs
@@ -8,7 +8,13 @@ const template = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
-    {{body}}
+    <div id="router">{{body}}</div>
+    <script type="module">
+    import WextRouter from './wext-client.js';
+
+    const routerElement = document.querySelector('#router');
+    const router = new WextRouter(routerElement);
+    </script>
   </body>
   </html>
 `;
@@ -19,7 +25,30 @@ const wextConfig = {
       {
         route: '/',
         template,
-        handler: async () => ({ body: `Hello`, head: '' })
+        handler: async () => ({
+          body: `
+            <h1>Wext.js</h1>
+            <h2>Home</h2>
+
+            <p>
+              Click link to go further!
+              Read <wext-link href="/about">about me.</wext-link>
+            </p>`,
+          head: '<title>Welcome</title>'
+        })
+      },
+      {
+        route: '/about',
+        template,
+        handler: async () => ({
+          body: `
+            <h1>Wext.js</h1>
+            <h2>About</h2>
+
+            <p>About me, heh? I'm rather cool I guess...</p>
+          `,
+          head: '<title>Cool person</title>'
+        })
       }
     ]
   }
