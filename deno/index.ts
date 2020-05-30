e0b5546e61e308c44816d1d78c3692d7f73e47f1
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2020-05-30 18:12:41 +0200

    Add support for Deno.

---
+++ deno/index.ts
@@ -0,0 +1,236 @@
+const { stat, open } = Deno;
+import { readFileStr } from 'https://deno.land/std@0.53.0/fs/read_file_str.ts';
+import { serve, ServerRequest, Response, Server } from 'https://deno.land/std@0.53.0/http/server.ts';
+import { extname } from "https://deno.land/std@0.53.0/path/mod.ts";
+
+interface PageData {
+  head: string;
+  body: string;
+};
+
+interface PageHandlerCallback {
+  (req: ServerRequest): Promise<PageData>;
+}
+
+interface Page {
+  route: string;
+  template: string;
+  handler: PageHandlerCallback;
+}
+
+interface Page {
+  route: string;
+  template: string;
+  handler: PageHandlerCallback;
+}
+
+interface RouterConfig {
+  pages: Page[];
+}
+
+interface ServerConfig {
+  compression: boolean;
+  serveStatic?: string;
+  minifyHTML: boolean;
+}
+
+interface WextConfig {
+  server: ServerConfig;
+  router: RouterConfig;
+}
+
+const MEDIA_TYPES: Record<string, string> = {
+  ".md": "text/markdown",
+  ".css": "text/css",
+  ".html": "text/html",
+  ".htm": "text/html",
+  ".json": "application/json",
+  ".map": "application/json",
+  ".txt": "text/plain",
+  ".ts": "text/typescript",
+  ".tsx": "text/tsx",
+  ".js": "application/javascript",
+  ".jsx": "text/jsx",
+  ".gz": "application/gzip",
+  ".webp": "image/webp",
+  ".jpg": "image/jpeg",
+  ".png": "image/png",
+  ".svg": "mage/svg+xml",
+};
+
+function getContentType(path: string): string | undefined {
+  return MEDIA_TYPES[extname(path)];
+}
+
+const defaultConfig: WextConfig = {
+  server: {
+    compression: true,
+    minifyHTML: true
+  },
+  router: {
+    pages: []
+  }
+};
+
+/**
+ * Function to decide wether or not we include the part
+ * of the template before the <wext-router> tag or not.
+ *
+ * If answerWithPartialContent if true, then anything
+ * before <wext-router> will not be sent in the request.
+ */
+function generatePreContent (template: string, answerWithPartialContent: boolean) {
+  return answerWithPartialContent ?
+    null :
+    template.indexOf('<wext-router>') !== -1 ? template.split('<wext-router>')[0] : template;
+}
+
+/**
+ * Function to decide wether or not we include the part
+ * of the template after the <wext-router> tag or not.
+ *
+ * If answerWithPartialContent if true, then anything
+ * after </wext-router> will not be sent in the request.
+ */
+function generatePostContent (template: string, answerWithPartialContent: boolean) {
+  return answerWithPartialContent ?
+    null :
+    template.indexOf('</wext-router>') !== -1 ? template.split('</wext-router>')[1] : template;
+}
+
+function reqToURL (req: ServerRequest) {
+  const base = req.conn.localAddr.transport === 'tcp' ? req.conn.localAddr.hostname : 'localhost';
+
+  return new URL(req.url, 'http://' + base);
+}
+
+async function wextProxy (req: ServerRequest, page: Page, config: WextConfig) {
+  const partialContent = Boolean(req.headers.get('x-partial-content') || reqToURL(req).searchParams.get('partialContent'));
+  const pageData = await page.handler(req);
+  const responseBody = [];
+
+  if (!pageData) {
+    throw new Error('Could not create PageData from handler.');
+  }
+
+  const { body, head } = pageData;
+
+  // body.replace('<wext-router></wext-router>', `<wext-router>${pageData.body}</wext-router>`)
+
+  const preContent = generatePreContent(page.template, partialContent);
+
+  const headers = new Headers();
+
+  headers.set('Cache-Control', 'public, max-age=3600');
+  headers.set('Content-Type', 'text/html');
+
+  /*
+    If we don't send preConent we still want to update the title in the header on client side navigations.
+    Send new title in X-Header-Updates.
+  */
+   if (!preContent && head) {
+    const match = head.match(/<title>(.+)<\/title>/i);
+
+    if (match) {
+      const title = match ? match[1] : '';
+      const json = JSON.stringify({ title });
+      const base64JSON = btoa(json);
+
+      headers.set('X-Header-Updates', base64JSON);
+    }
+  }
+
+  if (preContent) {
+    if (head) {
+      const preSplit = preContent.split(/<head>/);
+
+      responseBody.push(preSplit[0]);
+
+      // const headMarkup = '<head>' + (config.server.minifyHTML ? minifyHTML(head) : head);
+      const headMarkup = '<head>' + head;
+
+      responseBody.push(headMarkup);
+      responseBody.push(preSplit[1]);
+    } else {
+      await req.respond({ body: preContent });
+    }
+  }
+
+  // const mainBody = config.server.minifyHTML ? minifyHTML(body) : body;
+  const mainBody = body;
+
+  responseBody.push(partialContent ? mainBody : `<wext-router>${mainBody}</wext-router>`);
+
+  const postContent = generatePostContent(page.template, partialContent);
+
+  if (postContent) {
+    await responseBody.push(postContent);
+  }
+
+  await req.respond({ headers, body: responseBody.join('\n'), status: 200 });
+}
+
+async function serveStatic (req: ServerRequest, filePath: string) {
+  filePath = '.' + filePath;
+  const [file, fileInfo] = await Promise.all([open(filePath), stat(filePath)]);
+  const headers = new Headers();
+
+  headers.set('content-length', fileInfo.size.toString());
+
+  const contentType = getContentType(extname(filePath));
+
+  if (contentType) {
+    headers.set('content-type', contentType);
+  }
+
+  req.respond({ headers, body: file, status: 200 })
+}
+
+export default class Wext {
+  config: WextConfig;
+
+  constructor (config: WextConfig = defaultConfig) {
+    this.config = {
+      ...defaultConfig,
+      ...config
+    };
+
+    Object.freeze(this.config);
+  }
+
+  async handleRequests (server: Server) {
+    for await (const req of server) {
+      const url = reqToURL(req);
+      const page = this.config.router.pages.find(page => page.route === url.pathname);
+
+      const staticPath = url.pathname.match(this.config.server.serveStatic ?? '');
+
+      if (staticPath) {
+        await serveStatic(req, url.pathname);
+      } else if (url.pathname === '/wext-client.js') {
+        const body = await readFileStr('../browser/wext-client.js');
+
+        req.respond({
+          body,
+          headers: new Headers({
+            'Content-Type': 'application/javascript'
+          })
+        });
+      } else if (page) {
+        await wextProxy(req, page, this.config);
+      } else {
+        req.respond({ body: 'Not Found' });
+      }
+    }
+  }
+
+  async startServer (port = 5000) {
+    console.log('Using config: ', JSON.stringify(this.config));
+
+    const server = serve({ port });
+
+    console.log('Wext is running at: http://localhost:' + port);
+
+    await this.handleRequests(server);
+  }
+}
---
+++ deno/test.ts
@@ -0,0 +1,103 @@
+import Wext from './index.ts';
+
+const template = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <wext-router></wext-router>
+    <script type="module" src="wext-client.js"></script>
+  </body>
+  </html>
+`;
+
+const wextConfig = {
+  server: {
+    compression: true,
+    serveStatic: 'public',
+    minifyHTML: true
+  },
+  router: {
+    pages: [
+      {
+        route: '/',
+        template,
+        handler: async () => ({
+          body: `
+            <h1>Wext.js</h1>
+            <h2>Home</h2>
+
+            <figure>
+              <img src="public/blomster.webp" alt="Pinl tulips from the Botanical Garden in Oslo">
+              <figcaption>A picture of flowers from the Botanical Garden in Oslo</figcaption>
+            </figure>
+
+            <p>
+              Click link to go further!
+              Read <wext-link href="/about">about me.</wext-link>
+            </p>`,
+          head: '<title>Welcome</title>'
+        })
+      },
+      {
+        route: '/about',
+        template,
+        handler: async () => ({
+          body: `
+            <script src="web-comp.js" type="module"></script>
+            <h1>Wext.js</h1>
+            <h2>About</h2>
+
+            <hello-world></hello-world>
+
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+            <p>About me, heh? I'm rather cool I guess...</p>
+          `,
+          head: '<title>Hej - Åäö</title>'
+        })
+      }
+    ]
+  }
+};
+
+const wext = new Wext(wextConfig);
+
+await wext.startServer(5000);
